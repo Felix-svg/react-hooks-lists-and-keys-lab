196f56ae6f4510bacbe09276d4c66df177294fab
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-31 12:04:38 +0300

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
+// ProjectItem.js
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ id, name, about, technologies }) {
   return (
-    <div className="project-item">
+    <div className="project-item" key={id}>
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech, index) => (
+          <span key={index} className="technology">
+            {tech}
+          </span>
+        ))}
       </div>
     </div>
   );
